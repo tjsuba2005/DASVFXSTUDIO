1716c260d2f2dc8b45dee7e5f974372ef8b8df3d
suba <tjsuba2005@gmail.com>
2025-07-30 20:18:12 +0530

    client/vite.config.js is modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Add this line. It's the only change you need.
+  base: '/DASVFXSTUDIO/', 
+  
   plugins: [react()],
-    server: {
+  server: {
     hmr: {
       port: 5173, // Try explicitly setting the HMR port
     }
   }
 })
-

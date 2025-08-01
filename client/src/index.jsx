b2eaadfd919cd6cd2f7e09a723b2c3af4a18ef35
suba <tjsuba2005@gmail.com>
2025-08-01 17:15:41 +0530

    fix: Finalize Vite base path and routing for GH Pages

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,16 +4,17 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import './index.css'; // Global styles
 import { AuthProvider } from './context/AuthContext'; 
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <AuthProvider>
     <App/>
 
     </AuthProvider>
      
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );

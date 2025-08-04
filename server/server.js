7c5688d2fc7a12581248c644ed0430bb3fb2bbb6
suba <tjsuba2005@gmail.com>
2025-08-04 15:10:37 +0530

    update .git/COMMIT_EDITMSG

--- server/server.js
+++ server/server.js
@@ -62,9 +62,9 @@ app.use(session({
 // --- 3. Google OAuth2 Client & Scopes ---
 
 const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  process.env.VITE_GOOGLE_CLIENT_ID,
+  process.env.VITE_GOOGLE_CLIENT_SECRET,
+  process.env.VITE_GOOGLE_REDIRECT_URI
 );
 
 const scopes = [

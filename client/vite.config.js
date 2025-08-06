1e7974032c5e2cdc8b0d56beb5ef464fa7b99786
suba <tjsuba2005@gmail.com>
2025-08-06 14:18:41 +0530

    update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   // Add this line. It's the only change you need.
-  base: '/DASVFXSTUDIO/', 
+  base: '/', 
   
   plugins: [react()],
   server: {
